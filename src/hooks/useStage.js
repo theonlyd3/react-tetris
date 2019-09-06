ec2df6d0eb6b4c4e85e6fae6a92f096f8f1f3e0c
Deng Wuor Joak <dengjoak@gmail.com>
2019-09-06 20:37:50 +0200

    asd

--- src/hooks/useStage.js
+++ src/hooks/useStage.js
@@ -1,8 +1,32 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { createStage } from "../gameHelpers";
 
-export const useStage = () => {
+export const useStage = (player, resetplayer) => {
   const [stage, setStage] = useState(createStage());
+
+  useEffect(() => {
+    const updateStage = prevStage => {
+      // First flush the stage
+
+      const newStage = prevStage.map(row =>
+        row.map(cell => (cell[1] === "clear" ? [0, "clear"] : cell))
+      );
+
+      // Then draw the tetromino
+      player.tetromino.forEach((row, y) => {
+        row.forEach((value, x) => {
+          if (value !== 0) {
+            newStage[y + player.pos.y][x + player.pos.x] = [
+              value,
+              `${player.collied ? "merged" : "cleared"}`
+            ];
+          }
+        });
+      });
+    };
+
+    setStage(prev => updateStage(prev));
+  }, [player.collided, player.pos.x, player.pos.y, player.teromino]);
 
   return [stage, setStage];
 };
